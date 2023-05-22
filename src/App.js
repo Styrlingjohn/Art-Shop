f3d7a52c2fcb09c31940aeb47a6fbdfadc4edb96
Styrlingjohn <styrling_morris@yahoo.com>
2023-05-22 16:12:57 -0600

    added image scroll bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './App.css';
-import Website from "./components/Site";
+import Website from "./components/Website";
 
 
 
--- src/components/Sections/HomePage.js
+++ src/components/Sections/HomePage.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import ImageSlider from "./imageBar";
 
-function HomePage() {
+const HomePage = () => {
+  const slides = [
+    { url: "http://localhost:3000/beach.jpg", title: "beach" },
+    { url: "http://localhost:3000/waves.jpg", title: "waves" },
+    { url: "http://localhost:3000/mountain.png", title: "mountain" },
+    { url: "http://localhost:3000/valley.jpg", title: "valley" },
+    { url: "http://localhost:3000/desert.webp", title: "italy" },
+  ];
+  const containerStyles = {
+    width: "500px",
+    height: "280px",
+    margin: "0 auto",
+    backgroundColor: "#e0ffcd",
+  };
+  const headerStyle = {
+     textAlign: "center",
+  };
 
   return (
- 111
+    <div>
+
+      <div style={containerStyles}>
+        <h1 style={headerStyle}>Nature</h1>
+        <ImageSlider slides={slides} parentWidth={500} />
+      </div>
+    </div>
   );
-}
+};
 
 export default HomePage; 
